ad94ffb183203ad4369eb04edda9f0245c23e6e3
debrosky <christopherackom@gmail.com>
2018-06-20 15:56:35 +0000

    add ex2 to ex5

---
+++ Exercises/ex5.py
@@ -0,0 +1,26 @@
+#a bit about myself
+
+my_name = 'Abban Emmanuel'
+
+my_age = 23
+
+my_height = 65
+
+my_weight = 60
+
+my_eyes = 'indigo'
+
+my_teeth = 'White'
+
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+print "If I add %d, %d, and %d I get %d." % ( my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+print "i ate %s berries this morning" % my_hair
+
